784238b756b56d47931ebd4b9dd54302bf085575
Louis <louisedwardhess@icloud.com>
2021-09-08 14:53:09 -0700

    compression method finished

--- LZW Compression/src/LZWObject.java
+++ LZW Compression/src/LZWObject.java
@@ -1,18 +1,36 @@
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.HashMap;
 
 public class LZWObject {
-	private HashMap <Short, String> dictionary;
-	private File message;
-	public LZWObject(HashMap<Short, String> dictionary, File message) {
+	//get a string out of your original string; make a dictionary; convert the output string resultant from your output pattern to a byte array; print byte array to file using writeByteArrayToFile from apache
+	//try to maybe write to a .bin might make things easier
+	private static HashMap <Integer, String> dictionary;
+	private static File message;
+	public LZWObject(HashMap<Integer, String> dictionary, File message) {
 		super();
 		this.dictionary = dictionary;
 		this.message = message;
 	}
-	public static File encode() {
-		File output = new File("output.txt");
+	public static void encode() throws IOException {
+		FileReader reader = new FileReader(message);
+		String current = "" + (char)reader.read();
+		int codeOn = 256;
+		StringBuilder output = new StringBuilder("");
+		while (reader.ready()) {
+			if (!dictionary.containsKey(current)) {
+				output.append(current);
+				current = "" + (char)reader.read();
+			}
+			else {
+				dictionary.put(codeOn, current);
+				current = current + (char)reader.read();
+				codeOn++;
+			}
+		}
 		
-		return output;
 	}
 }
